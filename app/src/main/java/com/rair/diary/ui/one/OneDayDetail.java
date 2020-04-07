f8599dda96fe7f1dba04b9e062a6a6d36acad9d4
stan1812 <21314980309@qq.com>
2020-04-07 18:32:23 +0800

    初始化<One>详情页

---
+++ app/src/main/java/com/rair/diary/ui/one/OneDayDetail.java
@@ -0,0 +1,13 @@
+package com.rair.diary.ui.one;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OneDayDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one_day_detail);
+    }
+}
